c7c6e546c4bf9eacab0a33758b1dc5a53c8aac60
master <libaoming2@gmail.com>
2016-08-24 19:28:54 +0800

    complete phone loaction in coreLocation kit and use ClGeocoder reverse location , get placeMark info

--- LocationAware/ViewController.swift
+++ LocationAware/ViewController.swift
@@ -7,19 +7,82 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var country: UILabel!
+    @IBOutlet weak var adminCity: UILabel!
+    
+    @IBOutlet weak var speed: UILabel!
+    @IBOutlet weak var course: UILabel!
+    @IBOutlet weak var latitude: UILabel!
+    
+    @IBOutlet weak var longtitude: UILabel!
+    @IBOutlet weak var address: UILabel!
+    @IBOutlet weak var zipCode: UILabel!
+    
+
+    
+    var locationManger = CLLocationManager()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManger.delegate = self
+        locationManger.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locationManger.requestWhenInUseAuthorization()
+        locationManger.startUpdatingLocation()
+        
+        zipCode.text = ""
+        adminCity.text = ""
+        
+ 
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation = locations[0]
+        
+        latitude.text = "\(userLocation.coordinate.latitude)"
+        longtitude.text = "\(userLocation.coordinate.longitude)"
+        speed.text = "\(userLocation.speed)"
+        course.text = "\(userLocation.course)"
+        
+        
+        
+        CLGeocoder().reverseGeocodeLocation(userLocation) { (placeMark, error) in
+            if error  != nil {
+                print(error)
+            }else {
+                
+                
+                
+                if let placeMarkData = placeMark?[0] {
+                    
+                   
+                    if let countryData = placeMarkData.country {
+                        self.country.text = "\(countryData)"
+                    }
+                    
+                    if let zipCodeData = placeMarkData.postalCode {
+                        self.zipCode.text = "\(zipCodeData)"
+                    }
+                    
+                    if let sub = placeMarkData.thoroughfare, let sublocality = placeMarkData.subLocality   {
+                        self.address.text = "\(sub) \(sublocality)"
+                    }
+                    
+                    if let sub = placeMarkData.administrativeArea {
+                        self.adminCity.text = "\(sub)"
+                    }
+                    
+                    
+                }
+            }
+        }
     }
-
 
 }
 
